20aadb6509de7424b322fc6dfdaee1e7bb262d02
Legall-Campbell S (FCES) <30017931@students.southwales.ac.uk>
2021-12-14 11:39:08 +0000

    add textures

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,45 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
-    private int count; 
-    private float movementX;
-    private float movementY;
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-    }
+	//Referneces the score text
+	public TextMeshProUGUI countText;
 
-    void OnMove(InputValue movementValue)
+	//References the win screen text
+	public GameObject winTextObject;
 
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	private float movementX;
+	private float movementY;
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+	private Rigidbody rb;
+	private int count;
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+	//Only happens at start of game
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
 
-    }
+		count = 0;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Pickup"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-        }      
-    }
+		SetCountText();
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+
+			count = count + 1;
+
+			SetCountText();
+		}
+
+		//Restart game if the goose falls
+		if (other.gameObject.CompareTag("FallTag"))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		//Completes the game when the specified amount of items have been collected
+		if (count >= 21)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+		}
+	}
 }
--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -8,8 +8,7 @@ public class Spinner : MonoBehaviour
     void Update()
     {
         // Rotate the game object that this script is attached to by 15 in the X axis,
-        // 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
-        // rather than per frame.
+        // 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it every second instead of every frame.
 
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);        
     }
